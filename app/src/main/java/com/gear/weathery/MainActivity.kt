6ef21ef861118d34d80cb73ee31add896972cb21
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-11-19 19:23:00 +0100

    feat(MOB-6):implemented the splashactivity and onboarding flow

--- app/src/main/java/com/gear/weathery/MainActivity.kt
+++ app/src/main/java/com/gear/weathery/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.gear.weathery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.gear.weathery.dashboard.R.id.dashBoardFragment
+import com.gear.weathery.dashboard.R.id.dashboard_nav_graph
 import com.gear.weathery.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,8 +26,14 @@ class MainActivity : AppCompatActivity() {
 
         val fragHost = supportFragmentManager.findFragmentById(R.id.fragHost) as NavHostFragment
         navController = fragHost.findNavController()
+        var first  = intent.getBooleanExtra("FIRST",true)
 
-        setupActionBarWithNavController(navController)
+        if(!first){
+
+            navController.navigate(dashboard_nav_graph)
+           // navController.clearBackStack(dashboard_nav_graph)
+        }
+
     }
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
