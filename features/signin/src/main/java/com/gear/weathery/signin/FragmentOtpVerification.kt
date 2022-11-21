ac9986204c65741d745696e35aa95651d7db7103
Ella <ellalearnscopy@gmail.com>
2022-11-21 19:51:52 +0100

    fix: resolved bugs that crashed a few pages

--- features/signin/src/main/java/com/gear/weathery/signin/FragmentOtpVerification.kt
+++ features/signin/src/main/java/com/gear/weathery/signin/FragmentOtpVerification.kt
@@ -41,6 +41,7 @@ class FragmentOtpVerification : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_otp_verification, container, false)
+        binding = FragmentOtpVerificationBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
