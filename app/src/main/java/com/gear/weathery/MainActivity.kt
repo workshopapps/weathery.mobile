b3fac44bd5f37fdf4ae1f3bfec0ab4cff88750d4
kimsiele <sielekim.emmanuel@gmail.com>
2022-11-25 14:05:06 +0300

    feat(MOB-13):Implemented app settings, theme and Notification settings

--- app/src/main/java/com/gear/weathery/MainActivity.kt
+++ app/src/main/java/com/gear/weathery/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.gear.weathery
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.gear.weathery.dashboard.R.id.dashBoardFragment
-import com.gear.weathery.dashboard.R.id.dashboard_nav_graph
+import com.gear.weathery.common.preference.SettingsPreference
 import com.gear.weathery.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
@@ -18,8 +18,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
+    @Inject
+    lateinit var settingsPreference: SettingsPreference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            settingsPreference.darkMode().collect{ theme ->
+
+                when (theme) {
+                    getString(com.gear.weathery.setting.R.string.text_light) -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    }
+                    getString(com.gear.weathery.setting.R.string.text_dark) -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                    getString(com.gear.weathery.setting.R.string.text_system)-> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    }
+                }
+            }
+        }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -28,11 +48,11 @@ class MainActivity : AppCompatActivity() {
         navController = fragHost.findNavController()
         var first  = intent.getBooleanExtra("FIRST",true)
 
-        if(!first){
+        /*if(!first){
 
             navController.navigate(dashboard_nav_graph)
            // navController.clearBackStack(dashboard_nav_graph)
-        }
+        }*/
 
     }
     override fun onSupportNavigateUp(): Boolean {
