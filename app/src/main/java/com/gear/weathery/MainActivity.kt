e9d1e336db3f68a0e24bc3f5729eaba776e0eaa1
thadd <thaddeusojike@gmail.com>
2022-11-25 16:19:17 +0100

    chore(MOB-11): cleaned up files and code blocks no longer in use.

--- app/src/main/java/com/gear/weathery/MainActivity.kt
+++ app/src/main/java/com/gear/weathery/MainActivity.kt
@@ -19,9 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -37,8 +35,6 @@ class MainActivity : AppCompatActivity() {
             navController.navigate(dashboard_nav_graph)
            // navController.clearBackStack(dashboard_nav_graph)
         }
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
     }
     override fun onSupportNavigateUp(): Boolean {
