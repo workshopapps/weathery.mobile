905f56e8f8b16c43afa2613a15437ffb5ead1216
Ella <ellalearnscopy@gmail.com>
2022-11-25 14:54:08 +0100

    resolve merge conflicts

--- features/location/src/main/java/com/gear/add_remove_location/presentation/save_location/components/HourlyWeatherItem.kt
+++ features/location/src/main/java/com/gear/add_remove_location/presentation/save_location/components/HourlyWeatherItem.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,7 +15,7 @@ import com.gear.add_remove_location.R
 import com.gear.add_remove_location.presentation.ui.theme.Outfit
 
 @Composable
-fun HourlyWeatherItem() {
+fun HourlyWeatherItem(country: String, location: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -51,5 +52,5 @@ fun HourlyWeatherItem() {
 @Preview
 @Composable
 fun HPrev() {
-    HourlyWeatherItem()
+    HourlyWeatherItem("Nigeria", "Lagos")
 }
