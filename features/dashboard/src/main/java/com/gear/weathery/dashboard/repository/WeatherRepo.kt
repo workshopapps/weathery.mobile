e0727fef9c1a35645363d0e11675f0383144e767
thadd <thaddeusojike@gmail.com>
2022-12-02 10:09:02 +0100

    feat(MOB-54): implemented current forecast for dashboard

---
+++ features/dashboard/src/main/java/com/gear/weathery/dashboard/repository/WeatherRepo.kt
@@ -0,0 +1,141 @@
+package com.gear.weathery.dashboard.repository
+
+import com.gear.weathery.dashboard.models.DayWeather
+import com.gear.weathery.dashboard.models.HourWeather
+import com.gear.weathery.dashboard.models.WeatherCondition
+import com.gear.weathery.dashboard.network.GeoCodingNetworkApi
+import com.gear.weathery.dashboard.network.NetworkApi
+import org.json.JSONObject
+import java.text.ParsePosition
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+const val NONE = "none"
+
+object WeatherRepo {
+
+    suspend fun getWeatherForToday(lat: Double, long: Double): DayWeather {
+        return try {
+            getWeatherFromNetwork(lat, long)
+        } catch (e: Exception) {
+            generateMockDayWeather(lat, long)
+        }
+    }
+
+    private val weatherConditions = listOf(
+        "Rain showers",
+        "Rain",
+        "Thunderstorm",
+        "Clear sky",
+        "Scattered clouds",
+        "Few clouds",
+        "Broken clouds",
+        "Fog and depositing rime fog",
+        "Drizzle",
+        "Freezing Drizzle",
+        "Freezing rain",
+    )
+
+    private val weatherRisks = listOf(
+        "Thunderstorm",
+        "hail",
+        "Blizzard",
+        "Ice storm",
+        "Extreme heat",
+        "Flooding",
+        NONE,
+        NONE,
+        NONE,
+        NONE
+    )
+
+    private suspend fun generateMockDayWeather(lat: Double, long: Double): DayWeather {
+        val currentDateTime = Calendar.getInstance()
+
+        var stateName: String
+        var countryName: String
+        getStateAndCountryName(lat, long).also {
+            stateName = it.first
+            countryName = it.second
+        }
+
+        val currentMain = weatherConditions[(weatherConditions.indices).random()]
+        val currentRisk = weatherRisks[(weatherRisks.indices).random()]
+        val currentTime = currentDateTime.timeInMillis
+        val currentTimeEnd = currentTime + (60 * 60 * 1000)
+        val currentWeather = WeatherCondition(currentMain, currentRisk, currentTime, currentTimeEnd)
+
+        val currentHour = currentDateTime.get(Calendar.HOUR_OF_DAY)
+        val weatherTimeLine = mutableListOf<HourWeather>()
+        for (hour in currentHour..currentDateTime.getActualMaximum(Calendar.HOUR_OF_DAY)) {
+            val main = weatherConditions[(weatherConditions.indices).random()]
+            val risk = weatherRisks[(weatherRisks.indices).random()]
+            val timeInMillis = currentDateTime.timeInMillis
+            weatherTimeLine.add(HourWeather(main, risk, timeInMillis))
+            currentDateTime.roll(Calendar.HOUR_OF_DAY, true)
+        }
+
+        return DayWeather().apply {
+            state = stateName
+            country = countryName
+            this.currentWeather = currentWeather
+            timeLine = weatherTimeLine
+        }
+    }
+
+    private suspend fun getStateAndCountryName(lat: Double, long: Double): Pair<String, String> {
+        val jsonResponse = GeoCodingNetworkApi.geoCodingRetrofitService.getLocationName(lat, long)
+        val jsonObject = JSONObject(jsonResponse)
+        return parseJsonToLocationName(jsonObject)
+    }
+
+    private fun parseJsonToLocationName(jsonObject: JSONObject): Pair<String, String> {
+        val payLoadJsonObject = jsonObject.getJSONArray("geonames").getJSONObject(0)
+        val stateName = payLoadJsonObject.getString("adminName1")
+        val countryName = payLoadJsonObject.getString("countryName")
+        return Pair(stateName, countryName)
+    }
+
+    private suspend fun getWeatherFromNetwork(lat: Double, long: Double): DayWeather {
+        val jsonStringResponse = NetworkApi.retrofitService.getWeatherToday(lat, long)
+        val jsonObject = JSONObject(jsonStringResponse)
+        return parseJsonToTodayWeather(jsonObject)
+    }
+
+    private fun parseJsonToTodayWeather(jsonObject: JSONObject): DayWeather {
+        val dayWeather = DayWeather()
+        val state = jsonObject.getString("city")
+        val country = jsonObject.getString("country")
+
+        val currentInfo = jsonObject.getJSONObject("current")
+        val currentMain = currentInfo.getString("main")
+        val currentDateTime = getTimeInMillisFromString(currentInfo.getString("datetime"))
+        val currentEndDateTime = getTimeInMillisFromString(currentInfo.getString("end_datetime"))
+        val currentRisk = currentInfo.getString("risk")
+
+        val timelineArray = jsonObject.getJSONArray("today_timeline")
+        val hourWeathersList = mutableListOf<HourWeather>()
+        for (index in 0 until timelineArray.length()) {
+            val hourWeatherJSONObject = timelineArray.getJSONObject(index)
+            val main = hourWeatherJSONObject.getString("main")
+            val dateTime = getTimeInMillisFromString(hourWeatherJSONObject.getString("datetime"))
+            val risk = hourWeatherJSONObject.getString("risk")
+            hourWeathersList.add(HourWeather(main, risk, dateTime))
+        }
+
+        dayWeather.apply {
+            this.state = state
+            this.country = country
+            currentWeather =
+                WeatherCondition(currentMain, currentRisk, currentDateTime, currentEndDateTime)
+            timeLine = hourWeathersList
+        }
+
+        return dayWeather
+    }
+
+    private fun getTimeInMillisFromString(dateText: String): Long {
+        val date = SimpleDateFormat.getDateTimeInstance().parse(dateText, ParsePosition(0))
+        return date.time
+    }
+}
--- features/dashboard/src/main/java/com/gear/weathery/dashboard/ui/TimesWeatherRecyclerAdapter.kt
+++ features/dashboard/src/main/java/com/gear/weathery/dashboard/ui/TimesWeatherRecyclerAdapter.kt
@@ -4,23 +4,25 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import com.gear.weathery.dashboard.databinding.WeatherForTimesListItemBinding
-import com.gear.weathery.dashboard.models.UITimesWeather
-import com.gear.weathery.dashboard.ui.TimesWeatherRecyclerAdapter.TimesWeatherViewHolder
+import com.gear.weathery.dashboard.R
+import com.gear.weathery.dashboard.databinding.WeatherListBinding
+import com.gear.weathery.dashboard.models.HourWeather
+import com.gear.weathery.dashboard.models.getAmPmTime
+import com.gear.weathery.dashboard.repository.NONE
 
-class TimesWeatherRecyclerAdapter: RecyclerView.Adapter<TimesWeatherViewHolder>() {
+class TimelineRecyclerAdapter: RecyclerView.Adapter<TimelineRecyclerAdapter.HourWeatherViewHolder>() {
 
-    private var items = listOf<UITimesWeather>()
-    fun updateItemList(items: List<UITimesWeather>){
+    private var items = listOf<HourWeather>()
+    fun updateItemList(items: List<HourWeather>){
         this.items = items
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimesWeatherViewHolder {
-        return TimesWeatherViewHolder(WeatherForTimesListItemBinding.inflate(LayoutInflater.from(parent.context)))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HourWeatherViewHolder {
+        return HourWeatherViewHolder(WeatherListBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
-    override fun onBindViewHolder(holder: TimesWeatherViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: HourWeatherViewHolder, position: Int) {
         holder.bind(items[position])
     }
 
@@ -28,12 +30,23 @@ class TimesWeatherRecyclerAdapter: RecyclerView.Adapter<TimesWeatherViewHolder>(
         return items.size
     }
 
-    class TimesWeatherViewHolder(var binding: WeatherForTimesListItemBinding): ViewHolder(binding.root){
-        fun bind(uiTimesWeather: UITimesWeather){
+    class HourWeatherViewHolder(var binding: WeatherListBinding): ViewHolder(binding.root){
+        fun bind(hourWeather: HourWeather){
             binding.apply {
-                weatherTextView.text = uiTimesWeather.weather
-                weatherIconImageView.setImageResource(uiTimesWeather.iconResrcId)
-                timeTextView.text = uiTimesWeather.time
+                val time = getAmPmTime(hourWeather.timeInMillis)
+                timeTextView.text = "${time.first}:00\n${time.second}"
+                weatherDescriptionTextView.text = hourWeather.main
+                weatherRiskTextView.text = hourWeather.risk
+                weatherIconImageView.setImageResource(
+                    when(hourWeather.main){
+                        "Drizzle", "Freezing Drizzle", "Freezing rain" -> R.drawable.sun_cloud_rain
+                        "Rain", "Rain showers", "Thunderstorm" -> R.drawable.cloud_rain
+                        "Fog and depositing rime fog" -> R.drawable.cloud
+                        "Scattered clouds", "Few clouds", "Broken clouds" -> R.drawable.sun_cloud
+                        else -> R.drawable.sun
+                    }
+                )
+                riskIndicatorImageView.setImageResource(if (hourWeather.risk == NONE) R.drawable.ic_warning_inactive else R.drawable.ic_warning_active)
             }
         }
     }
